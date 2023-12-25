50db783128d86442dd781122aae0e8e53abdedda
Mashina2 <blknigga@mail.ru>
2023-12-25 18:16:53 +0300

    v1

---
+++ BstuGO/BstuGO/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BstuGO
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnButtonClicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new NewsPage());
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Schedule());
+        }
+    }
+}
---
+++ BstuGO/BstuGO/Schedule.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BstuGO
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class Schedule : ContentPage
+    {
+        WebView webView;
+        public Schedule()
+        {
+            InitializeComponent();
+            webView = new WebView
+            {
+                Source = new UrlWebViewSource { Url = "https://feis.bstu.by/wp-content/uploads/2023/08/%D0%A4%D0%AD%D0%98%D0%A1-3.pdf" },
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            this.Content = new StackLayout { Children = { webView } };
+        }
+    }
+}
